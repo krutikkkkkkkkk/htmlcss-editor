a569d4048d85b49d06adb593a28329421da3173d
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-06-22 08:57:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,26 +3,23 @@ slider.oninput = function() {
   document.getElementById('output').style.width = 100 - slider.value + "%"
   document.getElementById('coder').style.width = slider.value + "%"
 }
-function resize(clicked_id){
-    id = clicked_id;
 
-    if(id == "desktop"){
+document.getElementById('desktop').addEventListener('click', ()=> {
       document.getElementById('output').style.width = "75%"
       slider.value = 25
       document.getElementById('coder').style.width = "25%"
-    }
-    else if(id == "tablet"){
-      document.getElementById('output').style.width  = "50%"
-      slider.value = 50
-      document.getElementById('coder').style.width = "50%"
-    }
-    else if(id == "mobile"){
-      document.getElementById('output').style.width = "25%"
-      slider.value = 75
-      document.getElementById('coder').style.width = "75%"
-    }
-  }
+})
 
+document.getElementById('tablet').addEventListener('click', ()=> {
+    document.getElementById('output').style.width  = "50%"
+    slider.value = 50
+    document.getElementById('coder').style.width = "50%"
+})
+document.getElementById('mobile').addEventListener('click', ()=> {
+    document.getElementById('output').style.width = "25%"
+    slider.value = 75
+    document.getElementById('coder').style.width = "75%"
+})
 
 let Stop = document.getElementsByTagName('button')[1]
  Stop.addEventListener('click',()=>{
